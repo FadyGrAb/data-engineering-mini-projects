cfd90efe7a55659eb5f39e4a0bdec829981adc64
Fady <fady.michel@outlook.com>
2024-05-24 20:37:37 +0300

    working kafka and generator file

---
+++ mini-porjects/01-simple-streaming-visualization/alarms_generator_script/generate_alarms.py
@@ -0,0 +1,58 @@
+"""A network alarms simulator to be used as as source for a Kafka topic
+"""
+
+import random
+import json
+from time import sleep
+import datetime
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+
+# List of fictional alarms
+network_alarms = [
+    "Router Failure Detected - Core Router #1",
+    "High Latency - Data Center Switch #23",
+    "Unauthorized Access Attempt - Firewall #12",
+    "Packet Loss - Edge Router #5",
+    "CPU Overload - Load Balancer #3",
+    "Memory Leak - Server Cluster #7",
+    "Network Interface Down - Switch #19",
+    "DDoS Attack Detected - WAN Link #4",
+    "Firmware Outdated - Access Point #11",
+    "Power Supply Failure - UPS #2",
+    "Configuration Mismatch - Virtual Router #8",
+    "Temperature Threshold Exceeded - Data Center #9",
+    "Bandwidth Exceeded - ISP Link #6",
+    "Disk Space Critical - Storage Array #14",
+    "VPN Tunnel Failure - Remote Office #3",
+    "Authentication Failure - Radius Server #5",
+    "Service Outage - Email Server #2",
+    "SSL Certificate Expired - Web Server #10",
+    "Malware Detected - Endpoint #22",
+    "Redundancy Failure - Switch Stack #17",
+    "Broadcast Storm - VLAN #33",
+    "BGP Session Down - ISP Connection #1",
+    "ARP Spoofing Detected - Subnet #12",
+    "DNS Resolution Failure - Primary DNS #4",
+    "QoS Policy Violation - VoIP Gateway #9",
+]
+
+producer = KafkaProducer(bootstrap_servers="localhost:29092")
+
+while True:
+    payload = {
+        "timestamp": datetime.datetime.now().strftime("%Y%m%d %H:%M:%S"),
+        "node": "node_" + str(abs(int(random.normalvariate(3, 1).real))),
+        "alarm": random.choice(network_alarms),
+    }
+    future = producer.send(topic="network-alarms", value=json.dumps(payload).encode())
+    try:
+        record_metadata = future.get(timeout=10)
+        print(
+            f"Message delivered to {record_metadata.topic} partition {record_metadata.partition} offset {record_metadata.offset}"
+        )
+    except KafkaError as e:
+        print(f"Message delivery failed: {e}")
+
+    print(payload)
+    sleep(0.25)
