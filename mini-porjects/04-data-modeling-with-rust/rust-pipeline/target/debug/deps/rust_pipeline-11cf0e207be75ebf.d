465fd5e4799f2a00f8d0b12980a4d5c12ea6313c
Fady <fady.michel@outlook.com>
2024-07-11 20:07:45 +0300

    starting de with rust

---
+++ mini-porjects/04-data-modeling-with-rust/rust-pipeline/target/debug/deps/rust_pipeline-11cf0e207be75ebf.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/fady/code/data-engineering-mini-projects/mini-porjects/04-data-modeling-with-rust/rust-pipeline/target/debug/deps/librust_pipeline-11cf0e207be75ebf.rmeta: src/main.rs
+
+/mnt/c/Users/fady/code/data-engineering-mini-projects/mini-porjects/04-data-modeling-with-rust/rust-pipeline/target/debug/deps/rust_pipeline-11cf0e207be75ebf.d: src/main.rs
+
+src/main.rs:
